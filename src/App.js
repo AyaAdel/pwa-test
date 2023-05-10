b092d3edf07715c85e8ea2099d3196f6a64d978b
AyaAdel-tap <a.adel@tap.company>
2023-05-10 10:23:41 +0300

    IOS PWA

--- src/App.js
+++ src/App.js
@@ -1,47 +1,16 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import React from "react";
+import useIsIOS from "./useIsIOS";
+import {InstallPWA} from "./InstallPWA";
 
 function App() {
 
-  const [supportsPWA, setSupportsPWA] = useState(false);
-  const [promptInstall, setPromptInstall] = useState(null);
+  const { prompt } = useIsIOS();
 
-  useEffect(() => {
-    const handler = e => {
-      e.preventDefault();
-      console.log("we are being triggered :D");
-      setSupportsPWA(true);
-      setPromptInstall(e);
-    };
-    window.addEventListener("beforeinstallprompt", handler);
-
-    return () => window.removeEventListener("transitionend", handler);
-  }, []);
-
-  const onClick = evt => {
-    evt.preventDefault();
-    if (!promptInstall) {
-      return;
-    }
-    promptInstall.prompt();
-  };
-  // if (!supportsPWA) {
-  //   return null;
-  // }
   return (
-    <div className="App">
-      Hello
-      <button
-        className="link-button"
-        id="setup_button"
-        aria-label="Install app"
-        title="Install app"
-        onClick={onClick}
-      >
-        Install
-      </button>
-    </div>
-  );
+    <>
+      <h1>Hello PWA</h1>
+      {prompt && <InstallPWA />}
+    </>
+  )
 }
-
 export default App;
